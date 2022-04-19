2ceeb2f4a078f6e16e22d53392d29206e41dfe53
Tirath Tyagi <tirath.tyagi@student.amity.edu>
2022-04-19 18:51:49 +0530

    Lab-Programs

--- open_ended.py
+++ open_ended.py
@@ -1,34 +1,32 @@
-from collections import defaultdict,deque
-from itertools import combinations
-def min_cover(graph,n,m,tbv):
-    print("lol")
-    for i in range(len(tbv)):
-        for j in combinations(tbv,i):
-            print(j)
-if __name__ == '__main__':
-    m = int(input("Enter the total representatives of country A: "))
-    n = int(input("Enter the total representatives of country B: "))
-    k = int(input("Enter the total pairs chosen for delegation: "))
-    i = 0
-    l1 = list()
-    visited= defaultdict(list)
-    total_elems = deque()
-    while i<k:
-        p = input("Enter pairs: ").split()
-        p[0] = int(p[0])
-        p[1] = int(p[1])
-        if p[0] > m or p[1] > n:
-            print("INVALID PAIRS!!!!")
-            exit(1)
-        p[1] = chr(ord('@') + int(p[1]))
-        if p[0] not in total_elems:
-            total_elems.append(p[0])
-        if p[1] not in total_elems:
-            total_elems.append(p[1])
-        visited[p[0]].append(p[1])
-        visited[p[1]].append(p[0])
-        l1.append(p)
-        i = i+1
-    print(visited)
-    print(total_elems)
-    min_cover(visited,n,m,total_elems)
+def dfs(x):
+    for j in range(n+1):
+        if (v[j]!=1) and matrix[x][j] == 1:
+            v[j] = 1
+            if pre[j] == 0 or dfs(pre[j]):
+                pre[j] = i
+                return 1
+    return 0
+m = int(input("Enter the total representatives of country A: "))
+n = int(input("Enter the total representatives of country B: "))
+k = int(input("Enter the total pairs chosen for delegation: "))
+sol = m+n
+matrix = []
+pre = [0]*1100
+for i in range(m+1):
+    a = []
+    for j in range(n+1):
+        a.append(0)
+    matrix.append(a)
+i = 0
+while i < k:
+    s = input("Enter pairs: ").split()
+    l = int(s[0])
+    m = int(s[1])
+    matrix[l-1][m-1] = 1
+    i=i+1
+ans = 0
+for i in range(1,m+2):
+    v = [0]*1100
+    if dfs(i):
+        ans= ans+1
+print("Minimum number of telephone lines: ",sol-ans)
